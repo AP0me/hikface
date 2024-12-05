08acd26350ea01d914e8d102330f4d29df9bf68d
Anar <anar.abdullazada@ufaz.az>
2024-12-05 16:42:23 +0400

    batman

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="open-door.php"><button>Open</button></a>
+    <a href="close-door.php"><button>Close</button></a>
+</body>
+
+</html>
