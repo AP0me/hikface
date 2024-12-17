11472655b53cc8d8c5f852b5f0f8e19a0da6f2d8
Anar <anar.abdullazada@ufaz.az>
2024-12-17 11:51:57 +0400

    rearrange files

--- home-event.php
+++ home-event.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'hostname.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/deviceAPIs/logs.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/hostname.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/logs.php';
 
 // Call the function
 $logsTableValueMap = json_decode(file_get_contents('event-type-map.json'));
