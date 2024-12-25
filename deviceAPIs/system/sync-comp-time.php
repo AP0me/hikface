83e482b5b71638572ffbb40c37f8865d9e07c0af
Anar <anar.abdullazada@ufaz.az>
2024-12-25 17:28:55 +0400

    sync-comp-time

--- deviceAPIs/system/sync-comp-time.php
+++ deviceAPIs/system/sync-comp-time.php
@@ -1,5 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/hostname.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/helper/functions.php';
+
 function updateSystemTime($host, $xmlBody)
 {
   $url = "https://$host/ISAPI/System/time";
@@ -13,6 +15,7 @@ function updateSystemTime($host, $xmlBody)
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Bypass host verification
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); // Set method to PUT
   curl_setopt($ch, CURLOPT_POSTFIELDS, $xmlBody); // Set XML body
+  $ch = deviceAuth($ch);
 
   // Execute cURL request
   $response = curl_exec($ch);
@@ -21,7 +24,7 @@ function updateSystemTime($host, $xmlBody)
   if (curl_errno($ch)) {
     echo "cURL Error: " . curl_error($ch);
   } else {
-    echo "Response: " . $response;
+    return xmlToJson($response);
   }
 
   // Close cURL session
@@ -38,4 +41,4 @@ $xmlBody = '<?xml version="1.0" encoding="UTF-8"?>'
   . '</Time>';
 
 // Call the function
-updateSystemTime($host, $xmlBody);
+echo json_encode(updateSystemTime($host, $xmlBody));
